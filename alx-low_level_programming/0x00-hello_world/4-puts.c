c06861432a7a447db66165b5a3ebdec8562d1f2a
dahunsi-dami <gbemigadare@gmail.com>
2025-01-13 09:15:09 +0100

    Push solutions and their support files.

---
+++ alx-low_level_programming/0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	char *str2print = "\"Programming is like building a multilingual puzzle";
+
+	puts(str2print);
+
+	return (0);
+}
