47bc5a81643c1e427b6f1e41614bb5f533b2f33c
dahunsi-dami <gbemigadare@gmail.com>
2025-06-15 05:09:12 +0100

    Create & integrate math handler function & handle edge cases.

--- frontend/calculator/script.js
+++ frontend/calculator/script.js
@@ -1,20 +1,69 @@
 let buffer = "0";
+let lastOperation = null;
+let lastOperator = null;
 
 const btnContainer = document.querySelector(".button-container");
 const screen = document.querySelector(".screen");
 
+function mathHandler(symbol) {
+  console.log("last operator:", lastOperator);
+  if (lastOperator !== null) {
+    console.log("Other operation: ", buffer);
+
+    switch (lastOperator) {
+      case "÷":
+        lastOperation = lastOperation / buffer;
+        console.log("Divided");
+        break;
+      case "×":
+        lastOperation = lastOperation * buffer;
+        console.log("Multiplied");
+        break;
+      case "-":
+        lastOperation = lastOperation - buffer;
+        console.log("Subtracted");
+        break;
+      case "+":
+        lastOperation = lastOperation + buffer;
+        console.log("Added");
+        break;
+      default:
+        console.log("==== Wrong operator: '←' ====");
+        console.log("==== Reset with 'C' or press a number  ====");
+    }
+    if (lastOperation !== null && symbol !== "←") {
+      lastOperator = symbol;
+      buffer = "0";
+    }
+
+    if (symbol === "=") {
+      buffer = lastOperation.toString();
+    }
+  } else {
+    lastOperation = buffer;
+    lastOperator = symbol;
+    buffer = "0";
+    screen.innerText = buffer;
+  }
+}
+
 function symbolHandler(symbol) {
   if (symbol === "C") {
+    console.log("=== Resetting calculator ===");
     buffer = "0";
-  } else if (symbol === "←") {
+    lastOperation = null;
+    lastOperator = null;
+  } else if (symbol === "←" && lastOperation === null) {
     buffer = buffer.slice(0, -1);
   } else {
-    console.log("math op");
+    mathHandler(symbol);
   }
 
   if (buffer == "") {
     buffer = "0";
   }
+
+  console.log("last operation:", lastOperation);
 }
 
 btnContainer.addEventListener("click", (e) => {
@@ -26,6 +75,8 @@ btnContainer.addEventListener("click", (e) => {
     }
     buffer = target;
   } else if (buffer != "0" && !isNaN(target)) {
+    lastOperation = null;
+    lastOperator = null;
     buffer += target;
   } else {
     symbolHandler(target);
