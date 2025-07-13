00a5bbed207b1292d4222f7b3bee418c619748e4
dahunsi-dami <gbemigadare@gmail.com>
2025-07-13 04:47:37 +0100

    Do features minus word validation, loading spinner, and win animation.

--- frontend/my-wordle/scripts.js
+++ frontend/my-wordle/scripts.js
@@ -3,15 +3,17 @@ const spinner = document.querySelector(".spiral");
 const header = document.querySelector(".brand");
 const ANSWER_LENGTH = 5;
 let currentRow = 0;
-
+const dayWordUrl = "https://words.dev-apis.com/word-of-the-day";
+let dayWord = "";
 let buffer = "";
+let letterMatch = 0;
 
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
 function inputHandler(event) {
-  if (event.key === "Enter") {
+  if (event.key === "Enter" && buffer.length === ANSWER_LENGTH) {
     submit();
   } else if (event.key === "Backspace") {
     splicer();
@@ -24,9 +26,7 @@ function inputHandler(event) {
 
 function splicer() {
   if (buffer.length > 0) {
-    console.log("buffer before splice: ", buffer);
     buffer = buffer.slice(0, -1);
-    console.log("buffer after slice: ", buffer);
   }
   boxes[ANSWER_LENGTH * currentRow + buffer.length].textContent = "";
 }
@@ -41,13 +41,60 @@ function addLetter(letter) {
   boxes[ANSWER_LENGTH * currentRow + buffer.length - 1].textContent = letter.toUpperCase();
 }
 
-function submit() {
+async function getDayWord() {
+  const promise = await fetch(dayWordUrl);
+  const processedResponse = await promise.json();
+  dayWord = processedResponse.word.toUpperCase();
+  return dayWord;
+}
+
+function colorCoder() {
+  let dayWordCopy = dayWord.split("");
+  buffer = buffer.toUpperCase();
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    letterMatch = 0;
+    if (buffer[i] === dayWord[i]) {
+      boxes[ANSWER_LENGTH * currentRow + i].style.backgroundColor = "green";
+      boxes[ANSWER_LENGTH * currentRow + i].style.color = "white";
+      let letterIndex = dayWordCopy.indexOf(buffer[i]);
+      dayWordCopy.splice(letterIndex, 1);
+      letterMatch++;
+    }
+  }
+
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    if (dayWordCopy.includes(buffer[i]) && buffer[i] !== dayWord[i]) {
+      boxes[ANSWER_LENGTH * currentRow + i].style.backgroundColor = "orange";
+      boxes[ANSWER_LENGTH * currentRow + i].style.color = "white";
+      let letterIndex = dayWordCopy.indexOf(buffer[i]);
+      dayWordCopy.splice(letterIndex, 1);
+      letterMatch--;
+    } else if (buffer[i] !== dayWord[i] && !dayWordCopy.includes(buffer[i])) {
+      boxes[ANSWER_LENGTH * currentRow + i].style.backgroundColor = "gray";
+      boxes[ANSWER_LENGTH * currentRow + i].style.color = "white";
+      letterMatch--;
+    }
+  }
+}
+
+async function submit() {
   if (buffer.length === ANSWER_LENGTH) {
+    const dayWord = await getDayWord();
+    colorCoder();
     buffer = "";
-    // validate and color code later
   }
   currentRow++;
-  console.log("Current Row: ", currentRow);
+  setTimeout(() => {
+    if (currentRow === 6 && letterMatch !== 5) {
+      alert("you lose, the word was: " + dayWord);
+      document.removeEventListener("keydown", inputHandler);
+    } else {
+      if (letterMatch === 1 && currentRow <= 5) {
+        alert("you win");
+        document.removeEventListener("keydown", inputHandler);
+      }
+    }
+  }, 1);
 }
 
 function init() {
