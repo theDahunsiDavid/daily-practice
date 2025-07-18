b0d62cf4a647f25dc551bdb26dae70e4e4c0a7cf
dahunsi-dami <gbemigadare@gmail.com>
2025-07-19 00:55:25 +0100

    Fix flip animation and win/lose logic issue.

--- frontend/my-wordle/scripts.js
+++ frontend/my-wordle/scripts.js
@@ -59,70 +59,88 @@ async function getDayWord() {
   return dayWord;
 }
 
-function colorCoder() {
-  let dayWordCopy = dayWord.split("");
-  buffer = buffer.toUpperCase();
-  for (let i = 0; i < ANSWER_LENGTH; i++) {
-    letterMatch = 0;
-    if (buffer[i] === dayWord[i]) {
-      boxes[ANSWER_LENGTH * currentRow + i].classList.add("correct");
-      for (let keypad of onscreenKeyboard) {
-        if (keypad.innerText === buffer[i]) {
-          keypad.classList.add("correct");
-        }
-      }
-      let letterIndex = dayWordCopy.indexOf(buffer[i]);
-      dayWordCopy.splice(letterIndex, 1);
-      letterMatch++;
-    }
-  }
-
-  for (let i = 0; i < ANSWER_LENGTH; i++) {
-    if (dayWordCopy.includes(buffer[i]) && buffer[i] !== dayWord[i]) {
-      boxes[ANSWER_LENGTH * currentRow + i].classList.add("close");
-      let letterIndex = dayWordCopy.indexOf(buffer[i]);
-      dayWordCopy.splice(letterIndex, 1);
-      letterMatch--;
-    } else if (buffer[i] !== dayWord[i] && !dayWordCopy.includes(buffer[i])) {
-      boxes[ANSWER_LENGTH * currentRow + i].classList.add("wrong");
-      letterMatch--;
-    }
-  }
-}
-
 // function colorCoder() {
 //   let dayWordCopy = dayWord.split("");
 //   buffer = buffer.toUpperCase();
-//
-//   const rowToColor = currentRow;
+//   for (let i = 0; i < ANSWER_LENGTH; i++) {
+//     letterMatch = 0;
+//     if (buffer[i] === dayWord[i]) {
+//       boxes[ANSWER_LENGTH * currentRow + i].classList.add("correct");
+//       for (let keypad of onscreenKeyboard) {
+//         if (keypad.innerText === buffer[i]) {
+//           keypad.classList.add("correct");
+//         }
+//       }
+//       let letterIndex = dayWordCopy.indexOf(buffer[i]);
+//       dayWordCopy.splice(letterIndex, 1);
+//       letterMatch++;
+//     }
+//   }
 //
 //   for (let i = 0; i < ANSWER_LENGTH; i++) {
-//     setTimeout(() => {
-//       console.log("Current Row:", currentRow, "Index:", i);
-//       boxes[ANSWER_LENGTH * rowToColor + i].classList.add("flip");
-//
-//       setTimeout(() => {
-//         applyColorCoding(i, dayWordCopy, rowToColor);
-//       }, 300);
-//     }, i * 100);
-//   }
-// }
-//
-// function applyColorCoding(i, dayWordCopy, rowToColor) {
-//   if (buffer[i] === dayWord[i]) {
-//     boxes[ANSWER_LENGTH * rowToColor + i].classList.add("correct");
-//     // Update keyboard
-//     for (let keypad of onscreenKeyboard) {
-//       if (keypad.innerText === buffer[i]) {
-//         keypad.classList.add("correct");
-//       }
+//     if (dayWordCopy.includes(buffer[i]) && buffer[i] !== dayWord[i]) {
+//       boxes[ANSWER_LENGTH * currentRow + i].classList.add("close");
+//       let letterIndex = dayWordCopy.indexOf(buffer[i]);
+//       dayWordCopy.splice(letterIndex, 1);
+//       letterMatch--;
+//     } else if (buffer[i] !== dayWord[i] && !dayWordCopy.includes(buffer[i])) {
+//       boxes[ANSWER_LENGTH * currentRow + i].classList.add("wrong");
+//       letterMatch--;
 //     }
-//   } else if (dayWordCopy.includes(buffer[i]) && buffer[i] !== dayWord[i]) {
-//     boxes[ANSWER_LENGTH * rowToColor + i].classList.add("close");
-//   } else if (buffer[i] !== dayWord[i] && !dayWordCopy.includes(buffer[i])) {
-//     boxes[ANSWER_LENGTH * rowToColor + i].classList.add("wrong");
 //   }
 // }
+
+function colorCoder(onComplete) {
+  let dayWordCopy = dayWord.split("");
+  buffer = buffer.toUpperCase();
+
+  const capturedBuffer = buffer;
+  const capturedRowToColor = currentRow;
+
+  let completedAnimations = 0;
+
+  letterMatch = 0;
+
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    setTimeout(() => {
+      boxes[ANSWER_LENGTH * capturedRowToColor + i].classList.add("flip");
+
+      setTimeout(() => {
+        applyColorCoding(i, dayWordCopy, capturedRowToColor, capturedBuffer);
+
+        completedAnimations++;
+        if (completedAnimations === ANSWER_LENGTH && onComplete) {
+          setTimeout(() => {
+            onComplete();
+          }, 100);
+        }
+      }, 250);
+    }, i * 100);
+  }
+}
+
+function applyColorCoding(i, dayWordCopy, capturedRowToColor, capturedBuffer) {
+  if (capturedBuffer[i] === dayWord[i]) {
+    boxes[ANSWER_LENGTH * capturedRowToColor + i].classList.add("correct");
+
+    for (let keypad of onscreenKeyboard) {
+      if (keypad.innerText === capturedBuffer[i]) {
+        keypad.classList.add("correct");
+      }
+    }
+    let letterIndex = dayWordCopy.indexOf(capturedBuffer[i]);
+    dayWordCopy.splice(letterIndex, 1);
+    letterMatch++;
+  }
+
+  if (dayWordCopy.includes(capturedBuffer[i]) && capturedBuffer[i] !== dayWord[i]) {
+    boxes[ANSWER_LENGTH * capturedRowToColor + i].classList.add("close");
+    let letterIndex = dayWordCopy.indexOf(capturedBuffer[i]);
+    dayWordCopy.splice(letterIndex, 1);
+  } else if (capturedBuffer[i] !== dayWord[i] && !dayWordCopy.includes(capturedBuffer[i])) {
+    boxes[ANSWER_LENGTH * capturedRowToColor + i].classList.add("wrong");
+  }
+}
 
 async function checkIfWord() {
   setLoading(true);
@@ -152,22 +170,38 @@ function runRedAlert() {
 
 function submit() {
   if (buffer.length === ANSWER_LENGTH) {
-    colorCoder();
-    buffer = "";
-  }
-  currentRow++;
-  setTimeout(() => {
-    if (currentRow === 6 && letterMatch !== 5) {
-      alert("you lose, the word was: " + dayWord);
-      document.removeEventListener("keydown", inputHandler);
-    } else {
-      if (letterMatch === 1 && currentRow <= 5) {
-        alert("you win");
+    colorCoder(() => {
+      // console.log("Letter match after all animations: ", letterMatch);
+
+      if (currentRow === 6 && letterMatch !== ANSWER_LENGTH) {
+        // console.log("Letter match is: ", letterMatch);
+        alert("You lose, the word was: " + dayWord);
+        document.removeEventListener("keydown", inputHandler);
+      } else if (letterMatch === ANSWER_LENGTH) {
+        // console.log("Letter match is: ", letterMatch);
+        alert("You win!");
         document.querySelector(".brand").classList.add("winner");
         document.removeEventListener("keydown", inputHandler);
       }
-    }
-  }, 1000);
+    });
+    buffer = "";
+  }
+  currentRow++;
+  // console.log("Letter match before evaluation is: ", letterMatch);
+  // setTimeout(() => {
+  //   if (currentRow === 6 && letterMatch !== 5) {
+  //     console.log("Letter match is: ", letterMatch);
+  //     alert("you lose, the word was: " + dayWord);
+  //     document.removeEventListener("keydown", inputHandler);
+  //   } else {
+  //     if (letterMatch === 1 && currentRow <= 5) {
+  //       console.log("Letter match is: ", letterMatch);
+  //       alert("you win");
+  //       document.querySelector(".brand").classList.add("winner");
+  //       document.removeEventListener("keydown", inputHandler);
+  //     }
+  //   }
+  // }, 1000);
 }
 
 function setLoading(isLoading) {
